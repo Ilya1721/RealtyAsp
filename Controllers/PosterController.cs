6fbe4eb8ad9d31961d8d4c3b8b9426c4b3fe1dc4
Ilya1721 <ilyavusnya@gmail.com>
2020-04-21 21:52:15 +0300

    commit

--- Controllers/PosterController.cs
+++ Controllers/PosterController.cs
@@ -24,8 +24,106 @@ namespace Realty.Controllers
             ViewData["wishList"] = DataContext.WishFlatUserPivots
                                    .Where(wish => wish.UserName == User.Identity.Name)
                                    .ToList();
+            ViewData["currencies"] = DataContext.Currencies.ToList();
 
             return View();
+        }
+
+        public ActionResult Filter()
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-us");
+            int priceFrom;
+            int priceTo;
+            int currencyID;
+            int roomFrom;
+            int roomTo;
+
+            if(Request.Params["PriceFrom"] == "")
+            {
+                priceFrom = 0;
+            }
+            else
+            {
+                priceFrom = int.Parse(Request.Params["PriceFrom"]);
+                if(priceFrom < 0)
+                {
+                    priceFrom = 0;
+                }
+            }
+
+            if (Request.Params["PriceTo"] == "")
+            {
+                priceTo = 100000000;
+            }
+            else
+            {
+                priceTo = int.Parse(Request.Params["PriceTo"]);
+                if(priceTo < 0)
+                {
+                    priceTo = 0;
+                }
+            }
+
+            if (Request.Params["RoomFrom"] == "")
+            {
+                roomFrom = 1;
+            }
+            else
+            {
+                roomFrom = int.Parse(Request.Params["RoomFrom"]);
+                if(roomFrom < 0)
+                {
+                    roomFrom = 0;
+                }
+            }
+
+            if (Request.Params["RoomTo"] == "")
+            {
+                roomTo = 6;
+            }
+            else
+            {
+                roomTo = int.Parse(Request.Params["RoomTo"]);
+                if(roomTo < 0)
+                {
+                    roomTo = 0;
+                }
+            }
+
+            if (Request.Params["Currency"] == "")
+            {
+                currencyID = 1;
+            }
+            else
+            {
+                currencyID = int.Parse(Request.Params["Currency"]);
+            }
+
+            List<Flat> flats = DataContext.Flats.Where(f => f.CurrencyID == currencyID)
+                                                   .Where(f => f.RoomCount >= roomFrom)
+                                                   .Where(f => f.RoomCount <= roomTo).ToList();
+            if(priceTo == 0)
+            {
+                ViewData["posters"] = flats;
+            }
+            else if(DataContext.Currencies.Find(currencyID).Name == "$")
+            {
+                ViewData["posters"] = flats.Where(f => f.PriceDollar >= priceFrom)
+                                           .Where(f => f.PriceDollar <= priceTo).ToList();
+            }
+            else
+            {
+                ViewData["posters"] = flats.Where(f => f.PriceGrn >= priceFrom)
+                                           .Where(f => f.PriceGrn <= priceTo).ToList();
+            }
+                                                   
+            ViewData["photoPivots"] = DataContext.PhotoFlatPivots.ToList();
+            ViewData["wishList"] = DataContext.WishFlatUserPivots
+                                   .Where(wish => wish.UserName == User.Identity.Name)
+                                   .ToList();
+            ViewData["currencies"] = DataContext.Currencies.ToList();
+
+            return View("Index");
         }
 
         public ActionResult Show(int? posterID)
